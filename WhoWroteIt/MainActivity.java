539a2cab05dbce8e1b3b5210c12a12b4f041427e
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-04-10 20:11:16 -0400

    Create MainActivity.java

---
+++ WhoWroteIt/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.whowroteit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+    }
+
+    public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
+        new FetchBook(mTitleText, mAuthorText).execute(queryString);
+    }
+}
