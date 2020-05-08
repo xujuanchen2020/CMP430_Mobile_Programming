b94d7a7bf63d957b106efb2a523e33521ab5a862
xujuan_chen <60618009+xujuanchen2020@users.noreply.github.com>
2020-05-08 14:03:20 -0400

    Add files via upload

---
+++ Notebook/caloriecalculator2020/Note.java
@@ -0,0 +1,41 @@
+package com.example.caloriecalculator2020;
+
+public class Note {
+
+    private String title;
+    private String description;
+    private int priority;
+
+    public Note() {
+    }
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
